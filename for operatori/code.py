c4898161d4af292eb5374fc06f15324531b5ce2b
Javlonbek <uralovjavlonbek@yahoo.com>
2024-01-05 13:37:37 +0500

    7- Lesson
    for operatori bilan ishlash

---
+++ for operatori/code.py
@@ -0,0 +1,22 @@
+# maullif: Uralov Javlonbek
+
+# for sikli
+
+mehmonlar=["Anvar","Sarvar","Bobur"]
+
+for mehmon in mehmonlar :
+    print(mehmon ," assalom-u alaykum! ")
+    
+# for sonlar bilan
+
+sonlar=list(range(1,11))
+for son in sonlar:
+    print(son*son)
+    
+# royxatga ism kiritish
+
+dostlar=[]
+
+for n in range(1,5):
+    dostlar.append(input(f"{n}- dostingizning ismi kim ?"))
+
