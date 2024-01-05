9b2b5bee7456c462f6e1dc24708d7434903b2ac2
Javlonbek <uralovjavlonbek@yahoo.com>
2024-01-05 11:23:08 +0500

    1-lesson
    print funktsiyasidan foydalanish hamda arifmetik ammallar

---
+++ 1 print.arifmetik.ammallar/code.py
@@ -0,0 +1,14 @@
+#Bu yerda izoh yozilgan !
+#Muallif : Uralov Javlonbek
+print('Salom dunyo !')
+print("Salom dunyo !")
+print('Men kecha "ASUS" markali noutbook sotib oldim )') # "" ishlatish 
+print("Men kecha \"ASUS\" markali noutbook sotib oldim") # \ ishlatilinishi
+print("Qatorga\nbo\'lish")# qatorga bo`lish hamda ' dan foydalanish
+print(5*5-8)
+print(20/5)
+print(20//5) #butun songa keltirish
+print(19//5) #butun qismini olish
+print(5*4)
+print(5**4) # 5 ning 4 chi darajasi 
+print("9 ning kvadrati ",9**2," ga teng !")
