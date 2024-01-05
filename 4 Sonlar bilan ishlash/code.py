f2fb4081289f4940294f07edbc28bc8c9c297a4a
Javlonbek <uralovjavlonbek@yahoo.com>
2024-01-05 12:01:55 +0500

    4-Lesson
    Sonlar bilan ishlash

---
+++ 4 Sonlar bilan ishlash/code.py
@@ -0,0 +1,19 @@
+# Muallif :Javlonbek Uralov
+a=5
+b=5.5
+print(type(a)) # o`zgaruvchi tipini aniqlash
+aholi_soni=8_000_000_000 #katta sonlarni yozish
+print("yer aholisi soni ",aholi_soni)
+x,y,z=5,-5.4,5.5
+print(x)
+print(y)
+print(z)
+c=x*y
+print(c) 
+print(100/2)
+print(100//2)
+ism="Anvar"
+yosh=25
+print(ism, yosh," da")
+matn=ism+" "+ str(yosh)+" da" # str funktsiyasidan foydalanish
+print(matn)
