fc6d346670caf239e2566436cb925a23ad4e2e4e
Javlonbek <uralovjavlonbek@yahoo.com>
2024-01-05 15:29:23 +0500

    8-Lesson
    shart operatoriga oid kodlarim

---
+++ 8 Shart operatori/code.py
@@ -0,0 +1,33 @@
+#avtolar=['bmw','audi','mers','byd']
+
+#for avto in avtolar:
+#    if avto == 'byd':
+#        print(avto.upper())
+#    else:
+#        print(avto.title())
+
+#ism=input('ismingizni kiriting: ')
+    
+#if ism == 'ali':
+ #       print('salom ali')
+#else:
+#        print('siz ali emassiz')
+
+#yosh=int(input('yozingizni kiriting >>>'))
+#if yosh >18:
+ #   print('welcome')
+#else:
+ #   print('siz kira olmaysiz')  
+#if yosh<5 :
+ #   print('sizga tekin')
+#elif yosh<20 :
+ #   print('sizga 10 ming')
+#elif yosh>50:
+ #   print('sizga 11 ming')
+
+menu = ['palov','choy','kabob']
+ovqat=input('buyurtmani kiriting =')
+if ovqat.lower() in menu :
+    print('buyrtma qabul qilnidi !')
+else:
+    print('bunday ovqat yoq')
