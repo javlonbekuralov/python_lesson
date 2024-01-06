0d057aac2d5eec49e26274580b576c971dddfb2b
Javlonbek <uralovjavlonbek@yahoo.com>
2024-01-06 11:05:09 +0500

    12-Lesson

---
+++ 12 Funktsiya/code.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jan  6 10:04:36 2024
+
+@author: DDOS
+"""
+def salom_ber():
+    print('Assalomu alaykum !!!')
+salom_ber()
+
+def yuza(a,b):
+    print(a*b)
+yuza(5, 6)
+
+#qiymat qaytaruvchi funktsiya
+
+def yasa(ism,familya):
+    ism_fam=f"{ism} {familya}"
+    return(ism_fam)
+    
+sozlar=yasa('Javlonbek', 'Uralov')
+print(sozlar)
+
