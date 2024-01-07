52dba4d5ae86f92bd1b896f0531004d7dc2d26ba
Javlonbek <uralovjavlonbek@yahoo.com>
2024-01-07 09:09:24 +0500

    Lesson

--- 23 Funktsiya ro`yxat/code.py
+++ 23 Funktsiya ro`yxat/code.py
@@ -4,4 +4,15 @@ Created on Sat Jan  6 10:04:39 2024
 
 @author: DDOS
 """
-
+def bahola(ismlar):
+    baholar={}
+    while ismlar:
+        ism=ismlar.pop()
+        baho=input(f"Talaba {ism} ning bahosini kiriting :")
+        baholar[ism]=baho
+    return baholar
+    
+talabalar=['Ali','Vali','Anvar','Sarvar','Bobur']
+baholar=bahola(talabalar[:]) # royxat nusxasini jonatish 
+print(baholar)
+print(talabalar)
